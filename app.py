88ae02a5f8a5324a609ff38844122e1909398a4e
Zian Jiang <zj444@nyu.edu>
2021-09-05 06:28:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,16 +22,6 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
